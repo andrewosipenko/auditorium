5bd4552914624676e7aff33963ff39f6c207eac2
Andrew Osipenko <dev7000@gmail.com>
2018-12-03 21:21:46 +0300

    Generated project with https://start.spring.io/ Selected options: WEB, JPA, Security, H2, MySQL

---
+++ src/main/java/com/ao/auditorium/AuditoriumApplication.java
@@ -0,0 +1,12 @@
+package com.ao.auditorium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditoriumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditoriumApplication.class, args);
+	}
+}
